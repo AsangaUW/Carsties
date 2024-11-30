28d04091215dbed035df1ca84bb637db1ba2e638
AsangaUW <asangawork@gmail.com>
2024-11-30 16:52:44 -0500

    End of section 8

---
+++ frontend/web-app/app/auctions/CarImage.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import Image from 'next/image'
+import React, { useState } from 'react'
+
+type Props = {
+    imageUrl: string;
+    make: string;
+    model: string;
+    color: string;
+}
+
+export default function CarImage({ imageUrl, make, model, color }: Props) {
+    const [isLoading, setLoading] = useState(true);
+
+    return (
+        <Image
+            src={imageUrl}
+            alt={`image of ${make} ${model} ${color}`}
+            fill
+            priority
+            className={`
+                object-cover group-hover:opacity-75 duration-700 ease-in-out
+                ${isLoading ? 'grayscale blur-2xl scale-110' : 'grayscale-0 blur-0 scale-100'}
+            `}
+            sizes='(max-width: 768) 100vw, (max-width: 1200px) 50vw, 25vw'
+            onLoad={() => setLoading(false)}
+        />
+    )
+}
